c529be159f6da4931d0eab3227f72e26aa569582
Adriano Gotardo <adrianogotardo.12@gmail.com>
2021-08-16 16:51:55 -0300

    Beginning to implement styled-components in the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import SingleColor from './SingleColor'
 
+import styled from "styled-components";
 import Values from 'values.js'
 
 function App() {
@@ -24,10 +25,9 @@ function App() {
 
   return (
     <>
-    <section className="container">
+    <Title className="container">
       <h3>color generator</h3>
-    </section>
-    <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
       <input 
       type="text" 
       value={color} 
@@ -35,17 +35,49 @@ function App() {
       placeholder="#f15025"
       className={`${error?'error':null}`}
       ></input>
-      <button className="btn" type="submit">
+      <Button type="submit">
         submit
-      </button>
+      </Button>
     </form>
-    <section className="colors">
+    </Title>
+    <Colors className="colors">
       {list.map((color, index) => {
         return <SingleColor key={index} {...color} index={index} hexColor={color.hex}></SingleColor>
       })}
-    </section>
+    </Colors>
     </>
   )
 }
 
 export default App
+
+const Title = styled.section`
+  text-align: center;
+  display: flex;
+  align-items: center;
+  height: 100px;
+  padding-left: 2rem;
+  h3 {
+    margin-bottom: 0;
+    margin-right: 2rem;
+  }
+`;
+
+const Button = styled.button`
+  background: var(--clr-primary-5);
+  padding: 0.5rem 1rem;
+  font-size: 1.2rem;
+  border-color: transparent;
+  border-top-right-radius: var(--radius);
+  border-bottom-right-radius: var(--radius);
+  text-transform: capitalize;
+  color: var(--clr-white);
+  cursor: pointer;
+`;
+
+const Colors = styled.section`
+  min-height: calc(100vh - 100px);
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(223.33px, 1fr));
+  grid-template-rows: repeat(auto-fit, minmax(96px, 1fr));
+`;
